5b0e65b35e93b105905846b5ac8705f5d68740ce
Laxmi Narayan Pandey <l.n.p14112002@gmail.com>
2025-03-19 15:43:32 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -24,32 +24,5 @@ export default defineConfig({
         global: 'globalThis',
       },
     },
-    include: ['web3', '@web3modal/ethereum', '@web3modal/react', 'wagmi', 'viem']
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          mui: ['@mui/material', '@mui/icons-material'],
-          web3: ['web3', '@web3modal/ethereum', '@web3modal/react', 'wagmi', 'viem']
-        }
-      }
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  }
 })
